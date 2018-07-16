33b6220f92de7ca1b10a8bc04d6781719e288f39
Sam Galizia <samgalizia@gmail.com>
2018-07-16 15:01:12 -0700

    Finished addKey command

--- src/commands.js
+++ src/commands.js
@@ -1,15 +1,36 @@
 /* eslint-disable-next-line import/no-unresolved */
-const vscode = require('vscode');
+const { window } = require('vscode');
 
-exports.addKey = () => {
+const storedAPIKey = 'vscode-gists.apiKey';
+
+exports.addKey = (context) => {
   // Display a message box to the user
-  vscode.window.showInformationMessage('Hello World!');
+  const inputOptions = {
+    placeholder: 'Personal Access Token',
+    prompt: `Please go to GitHub.com, Settings -> Developer Settings -> Personal Access Tokens. Click 'Generate new token'.
+    Enter a description, such as 'vscode-gists-token', and check the permission for gist. Generate the token and then paste
+    the value into the box above.`,
+    ignoreFocusOut: true,
+  };
+  window.showInputBox(inputOptions).then((token) => {
+    if (token === undefined || token.trim() === '') {
+      return window.showErrorMessage('Your token cannot be empty. Please try entering it again.');
+    }
+
+    return context.globalState.update(storedAPIKey, token)
+      .then(() => {
+        window.showInformationMessage('API token has been saved. You are now good to go!');
+      })
+      .catch((err) => {
+        window.showErrorMessage(`An error has occured: ${err}`);
+      });
+  });
 };
 
-exports.savePublicGist = () => {
-  vscode.window.showInformationMessage('Saved as Public Gist');
+exports.savePublicGist = (context) => {
+  window.showInformationMessage(`Your key is: ${context.globalState.get(storedAPIKey)}`);
 };
 
-exports.savePrivateGist = () => {
-  vscode.window.showInformationMessage('Saved as Private Gist');
+exports.savePrivateGist = (context) => {
+  window.showInformationMessage(`Your key is: ${context.globalState.get(storedAPIKey)}`);
 };
--- src/extension.js
+++ src/extension.js
@@ -1,19 +1,22 @@
 /* eslint-disable-next-line import/no-unresolved */
-const vscode = require('vscode');
+const { commands } = require('vscode');
 const { addKey, savePublicGist, savePrivateGist } = require('./commands');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 exports.activate = function activate(context) {
-  // This line of code will only be executed once when your extension is activated
+  // Log when extension is activated
   console.log('vscode-gists is now active');
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('extension.addKey', addKey),
-    vscode.commands.registerCommand('extension.savePublicGist', savePublicGist),
-    vscode.commands.registerCommand('extension.savePrivateGist', savePrivateGist),
+    commands.registerCommand('extension.addKey', () => addKey(context)),
+    commands.registerCommand('extension.savePublicGist', () => savePublicGist(context)),
+    commands.registerCommand('extension.savePrivateGist', () => savePrivateGist(context)),
   );
 };
 
 // this method is called when your extension is deactivated
-exports.deactivate = function deactivate() { };
+exports.deactivate = function deactivate() {
+  // Log when extension deactivates, make sure this is last line called
+  console.log('vscode-gists is now deactivated.');
+};
